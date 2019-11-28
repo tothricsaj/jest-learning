de616334897936edceb6545d2691f3d6f8c00d26
tothricsaj <tothricsaj@gmail.com>
2019-11-28 16:29:21 +0100

    webpack config/npm-debug log is in gitignore

---
+++ webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    entry: {
+        main: "./src/index.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // 3. Turns css into DOM
+                    "css-loader", // 2. Turns css into commonjs
+                    "sass-loader" // 1. Turns sass into css
+                ]
+            }
+        ]
+    },
+});
