a8a2b10fccd93abdac86fb011318db19dd8b1357
tothricsaj <tothricsaj@gmail.com>
2019-11-29 11:09:46 +0100

    :zap: get started and working :wink:

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,3 @@
 export default function run() {
-    let divElement = `
-        <div class="test-elem">This is a test elem</div>
-    `;
-    document.body.insertAdjacentElement('beforeend', divElement);
+    console.log('from app.js');
 }
