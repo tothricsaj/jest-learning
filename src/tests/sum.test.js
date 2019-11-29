2fe35bc2dbdb1bcdc31a9d794f844735ad1f9029
tothricsaj <tothricsaj@gmail.com>
2019-11-29 11:34:35 +0100

    truthy staff :bus:

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,5 +1,33 @@
 const sum = require('../app/sum.js');
 
+/*
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('object assignment', () => {
+    const data = {one: 1};
+    data.two = 2;
+    const arr = ['sas', 'kacsa'];
+    expect(data).toEqual({one: 1, two: 2});
+    expect(arr).toEqual(['sas', 'kacsa']);
+});
+*/
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
 });
