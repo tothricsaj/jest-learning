a553378d38f374e43fad01158fd26961b98ad5d5
tothricsaj <tothricsaj@gmail.com>
2019-12-04 15:48:09 +0100

    order :sunflower:

--- src/tests/scoping.test.js
+++ src/tests/scoping.test.js
@@ -1,3 +1,4 @@
+/*
 let counter = 0;
 let city;
 let food;
@@ -68,3 +69,35 @@ describe('scope staff', () => {
         expect(foodFunc(city, food)).toBe('kitty');
     });
 });
+*/
+
+
+describe('outer', () => {
+  console.log('describe outer-a');
+
+  describe('describe inner 1', () => {
+    console.log('describe inner 1');
+    test('test 1', () => {
+      console.log('test for describe inner 1');
+      expect(true).toEqual(true);
+    });
+  });
+
+  console.log('describe outer-b');
+
+  test('test 1', () => {
+    console.log('test for describe outer');
+    expect(true).toEqual(true);
+  });
+
+  describe('describe inner 2', () => {
+    console.log('describe inner 2');
+    test('test for describe inner 2', () => {
+      console.log('test for describe inner 2');
+      expect(false).toEqual(false);
+    });
+  });
+
+  console.log('describe outer-c');
+});
+
