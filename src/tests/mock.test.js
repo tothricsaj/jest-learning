9afa2b61abecc963b7474690643f6d3ed84f77a5
tothricsaj <tothricsaj@gmail.com>
2019-12-05 16:32:20 +0100

    explore :wink:

--- src/tests/mock.test.js
+++ src/tests/mock.test.js
@@ -5,12 +5,16 @@ function trzForEach(items, callback) {
 }
 
 const mockCallback = jest.fn(x => 42 + x);
-trzForEach([0,1], mockCallback);
+trzForEach([2,1], mockCallback);
 
 test('mock practice', () => {
     expect(mockCallback.mock.calls.length).toBe(2);
-    expect(mockCallback.mock.calls[0][0]).toBe(0);
+    expect(mockCallback.mock.calls[0][0]).toBe(2);
     expect(mockCallback.mock.calls[1][0]).toBe(1);
-    expect(mockCallback.mock.results[0].value).toBe(42);
+    expect(mockCallback.mock.results[0].value).toBe(44);
+    expect(mockCallback.mock.results[1].value).toBe(43);
+
+    console.log(mockCallback.mock.results[1]);
+    console.log(mockCallback.mock);
 });
 
